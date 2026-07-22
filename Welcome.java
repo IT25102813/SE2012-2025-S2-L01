95129219f563e1f9c186cf24832086d8a556d877
IT25102813 <IT25102813@my.sliit.lk>
2026-07-22 14:50:23 +0530

    act 1 finished

---
+++ Welcome.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Welcome{
+        public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        //Getting input - first name
+        System.out.print("Enter your first name ");
+        String f_name = scanner.nextLine();
+
+        //Getting input - last name
+        System.out.print("Enter your last name ");
+        String l_name = scanner.nextLine();
+
+        //printing output
+        System.out.printf("Welcome to Second Year %s %s\n", f_name, l_name);
+        }
+}
+
