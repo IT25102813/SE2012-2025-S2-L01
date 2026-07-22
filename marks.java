03667eba355bb6e37149cf91da64effd63011cd2
IT25102813 <IT25102813@my.sliit.lk>
2026-07-22 15:27:24 +0530

    ...... act 2

--- marks.java
+++ marks.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class marks{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+	
+		System.out.print("Enter number of students");
+		int num_of_students = scanner.nextInt();
+		
+		int array[n][3]; //[][1] - Mathematics
+				 //[][2] - Chemistry
+				 //[][3] - Physics
 
+		System.out.print("--Menu--");
+		int choice = scanner.nextInt();
+
+		switch(choice){
+			case 1:
+
+		}
 	}
 }
